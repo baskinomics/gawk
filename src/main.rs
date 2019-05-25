ccf5d1008cb06efcde44bc6470cfd704b174ff6d
Sean Baskin <seanbaskin@gmail.com>
2019-05-25 14:06:51 -0400

    Added initial learning implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+/// todo documentation
+mod domain {
+    /// todo documentation
+    pub mod model {
+        /// Represents the YAML configuration file provided by the user.
+        pub struct Config {
+            /// Collection of `String` representing the path to a git repository on disk.
+            pub repositories: Vec<String>,
+        }
+    }
+}
+
+/// todo documentation
 fn main() {
-    println!("Hello, world!");
+    // Neato
+    let config = domain::model::Config {
+        repositories: vec!["test/one".to_string(), "test/two".to_string()],
+    };
+
+    // Noob
+    let repos: String = config.repositories.into_iter().collect();
+    println!("Repositories: {}", repos);
 }
